cb691132e217e5a5b6420ce9f8e60c1b7362781b
khldoon <oi18aan@cs.umu.se>
2021-10-14 14:39:52 +0200

    created content for Favourites.js and More.js

--- app/components/Favourites.js
+++ app/components/Favourites.js
@@ -1,15 +1,131 @@
 import * as React from "react";
-import { Avatar, Card, IconButton } from "react-native-paper";
+import {
+	Avatar,
+	Card,
+	IconButton,
+	Paragraph,
+	Subtitle,
+	Icon,
+} from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 
-const Favorits = () => (
-	<Card.Title
-		title="Card Title"
-		subtitle="Card Subtitle"
-		left={(props) => <Avatar.Icon {...props} icon="folder" />}
-		right={(props) => (
-			<IconButton {...props} icon="more-vert" onPress={() => {}} />
-		)}
-	/>
-);
+class Favorits extends React.Component {
+	render() {
+		return (
+			<View>
+				<Card>
+					<Card.Title
+						style={styles.cardContainer}
+						title="Umeå University"
+						subtitle="Universitetstorget 4, 907 36 Umeå"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="heart"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+					<Card.Content>
+						<Paragraph style={styles.cardParagraph}>
+							Estimated crowdedness: 85%{" "}
+						</Paragraph>
+					</Card.Content>
+				</Card>
+				<Card>
+					<Card.Title
+						style={styles.cardContainer}
+						title="ICA"
+						subtitle="Ålidhem 4, 907 35 Umeå"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="heart"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+					<Card.Content>
+						<Paragraph style={styles.cardParagraph}>
+							Estimated crowdedness: 85%{" "}
+						</Paragraph>
+					</Card.Content>
+				</Card>
+				<Card>
+					<Card.Title
+						style={styles.cardContainer}
+						title="Åhlens"
+						subtitle="Universitetstorget 4, 907 36 Umeå"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="heart"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+					<Card.Content>
+						<Paragraph style={styles.cardParagraph}>
+							Estimated crowdedness: 85%{" "}
+						</Paragraph>
+					</Card.Content>
+				</Card>
+				<Card>
+					<Card.Title
+						style={styles.cardContainer}
+						title="Lidl"
+						subtitle="Universitetstorget 4, 907 36 Umeå"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="heart"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+					<Card.Content>
+						<Paragraph style={styles.cardParagraph}>
+							Estimated crowdedness: 85%{" "}
+						</Paragraph>
+					</Card.Content>
+				</Card>
 
+				<Card>
+					<Card.Title
+						style={styles.cardContainer}
+						title="Coop nära"
+						subtitle="Universitetstorget 4, 907 36 Umeå"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="heart"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+					<Card.Content>
+						<Paragraph style={styles.cardParagraph}>
+							Estimated crowdedness: 85%{" "}
+						</Paragraph>
+					</Card.Content>
+				</Card>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	avatarIcon: {
+		backgroundColor: "#ffff",
+	},
+
+	cardParagraph: {
+		marginLeft: 55,
+	},
+});
 export default Favorits;
--- app/components/More.js
+++ app/components/More.js
@@ -1,12 +1,95 @@
-import React from "react";
-import { View, Text } from "react-native";
-const More = () => {
-	return (
-		<View
-			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-		>
-			<Text>More!</Text>
-		</View>
-	);
-};
+import * as React from "react";
+import { Avatar, Card } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+
+class More extends React.Component {
+	render() {
+		return (
+			<View>
+				<Card style={styles.cardContainer}>
+					<Card.Title
+						title="RATE US"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="star-outline"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+				</Card>
+
+				<Card style={styles.cardContainer}>
+					<Card.Title
+						title="ABOUT US"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="information-outline"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+				</Card>
+
+				<Card style={styles.cardContainer}>
+					<Card.Title
+						title="CONTACT US"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="account-box-outline"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+				</Card>
+
+				<Card style={styles.cardContainer}>
+					<Card.Title
+						title="SUPPORT US"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="gift-outline"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+				</Card>
+
+				<Card style={styles.cardContainer}>
+					<Card.Title
+						title="SETTINGS"
+						left={(props) => (
+							<Avatar.Icon
+								style={styles.avatarIcon}
+								{...props}
+								icon="cog-outline"
+								color="black"
+							/>
+						)}
+					></Card.Title>
+				</Card>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	avatarIcon: {
+		backgroundColor: "#ffff",
+	},
+
+	cardParagraph: {
+		marginLeft: 55,
+	},
+	cardContainer: {
+		shadowOpacity: 5,
+	},
+});
 export default More;
